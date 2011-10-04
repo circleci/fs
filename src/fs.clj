9f4615004b70918d209cbea032ef8741fb781f51
Miki Tebeka <miki.tebeka@gmail.com>
2011-10-04 13:37:20 -0700

    Ignore lein test history

---
+++ src/fs.clj
@@ -0,0 +1,307 @@
+(ns ^{:doc "File system utilities in Clojure"
+      :author "Miki Tebeka <miki.tebeka@gmail.com>"}
+  fs
+  (:require [clojure.zip :as zip]
+            [clojure.java.io :as io])
+  (:import java.io.File
+           java.io.FilenameFilter))
+
+; File separator
+(def ^:dynamic *separator* File/separator)
+; Extension separator
+(def ^:dynamic *extension-separator* ".")
+
+(defn listdir
+  "List files under path."
+  [path]
+  (seq (.list (io/as-file path))))
+
+(defn executable?
+  "Return true if path is executable."
+  [path]
+  (.canExecute (io/as-file path)))
+
+(defn readable?
+  "Return true if path is readable."
+  [path]
+  (.canRead (io/as-file path)))
+
+(defn writeable?
+  "Return true if path is writeable."
+  [path]
+  (.canWrite (io/as-file path)))
+
+(defn delete
+  "Delete path. Returns path."
+  [path]
+  (.delete (io/as-file path))
+  path)
+
+(defn exists?
+  "Return true if path exists."
+  [path]
+  (.exists (io/as-file path)))
+
+(defn abspath
+  "Return absolute path."
+  [path]
+  (.getAbsolutePath (io/as-file path)))
+
+(defn- strinfify [file]
+  (.getCanonicalPath file))
+
+(defn normpath
+  "Return nomralized (canonical) path."
+  [path]
+  (strinfify (io/as-file path)))
+
+(defn basename
+  "Return basename (file part) of path.\n\t(basename \"/a/b/c\") -> \"c\"."
+  [path]
+  (.getName (io/as-file path)))
+
+(defn extension
+  "Return the file extension.\n\t(extension \"fs.clj\") -> \".clj\"."
+  [path]
+  (let [base (basename path)
+        i (.lastIndexOf base *extension-separator*)]
+    (if (pos? i)
+      (subs base i)
+      "")))
+
+(defn dirname
+  "Return directory name of path.\n\t(dirname \"a/b/c\") -> \"/a/b\"."
+  [path]
+  (.getParent (io/as-file path)))
+
+(defn directory?
+  "Return true if path is a directory."
+  [path]
+  (.isDirectory (io/as-file path)))
+
+(defn file?
+  "Return true if path is a file."
+  [path]
+  (.isFile (io/as-file path)))
+
+(defn mtime
+  "Return file modification time."
+  [path]
+  (.lastModified (io/as-file path)))
+
+(defn size
+  "Return size (in bytes) if file."
+  [path]
+  (.length (io/as-file path)))
+
+(defn mkdir
+  "Create a directory. Returns path."
+  [path]
+  (.mkdir (io/as-file path))
+  path)
+
+(defn mkdirs
+  "Make directory tree. Returns path."
+  [path]
+  (.mkdirs (io/as-file path))
+  path)
+
+(defn join
+  "Join parts of path.\n\t(join [\"a\" \"b\"]) -> \"a/b\"."
+  [& parts]
+  (apply str (interpose *separator* parts)))
+
+(defn split
+  "Split path to componenets.\n\t(split \"a/b/c\") -> (\"a\" \"b\" \"c\")."
+  [path]
+  (into [] (.split path *separator*)))
+
+(defn rename
+  "Rename old-path to new-path."
+  [old-path new-path]
+  (.renameTo (io/as-file old-path) (io/as-file new-path)))
+
+(defn- ensure-file [path]
+  (let [file (io/as-file path)]
+    (when-not (.exists file) (.createNewFile file))
+    file))
+
+(defn- assert-exists [path]
+  (when-not (exists? path)
+    (throw (IllegalArgumentException. (str path " not found")))))
+
+(defn copy
+  "Copy a file from 'from' to 'to'. Return 'to'."
+  [from to]
+  (assert-exists from)
+  (io/copy (io/as-file from) (io/as-file to))
+  to)
+
+; FIXME: Have prefix, suffix and directory keword arguements and add
+; delete-on-exit
+(defn tempfile 
+  "Create a temporary file."
+  ([] (tempfile "-fs-" ""))
+  ([prefix] (tempfile prefix ""))
+  ([prefix suffix] (.getAbsolutePath (File/createTempFile prefix suffix)))
+  ([prefix suffix directory] 
+   (.getAbsolutePath 
+     (File/createTempFile prefix suffix (io/as-file directory)))))
+
+(defn tempdir
+  "Create a temporary directory."
+  ([] (let [dir (File/createTempFile "-fs-" "")
+            path (.getAbsolutePath dir)]
+        (.delete dir)
+        (.mkdir dir)
+        path))
+  ([root]
+   (let [dir (File/createTempFile "-fs-" "" (io/as-file root))
+         path (.getAbsolutePath dir)]
+     (.delete dir)
+     (.mkdir dir)
+     path)))
+
+(defn cwd
+  "Return the current working directory."
+  []
+  (abspath "."))
+
+; Taken from https://github.com/jkk/clj-glob. (thanks Justin!)
+(defn- glob->regex
+  "Takes a glob-format string and returns a regex."
+  [s]
+  (loop [stream s
+         re ""
+         curly-depth 0]
+    (let [[c j] stream]
+        (cond
+         (nil? c) (re-pattern 
+                    ; We add ^ and $ since we check only for file names
+                    (str "^" (if (= \. (first s)) "" "(?=[^\\.])") re "$"))
+         (= c \\) (recur (nnext stream) (str re c c) curly-depth)
+         (= c \/) (recur (next stream) (str re (if (= \. j) c "/(?=[^\\.])"))
+                         curly-depth)
+         (= c \*) (recur (next stream) (str re "[^/]*") curly-depth)
+         (= c \?) (recur (next stream) (str re "[^/]") curly-depth)
+         (= c \{) (recur (next stream) (str re \() (inc curly-depth))
+         (= c \}) (recur (next stream) (str re \)) (dec curly-depth))
+         (and (= c \,) (< 0 curly-depth)) (recur (next stream) (str re \|)
+                                                 curly-depth)
+         (#{\. \( \) \| \+ \^ \$ \@ \%} c) (recur (next stream) (str re \\ c)
+                                                  curly-depth)
+         :else (recur (next stream) (str re c) curly-depth)))))
+
+(defn glob
+  "Returns files matching glob pattern."
+  [pattern]
+  (let [parts (split pattern)
+        root (if (= (count parts) 1) "." (apply join (butlast parts)))
+        regex (glob->regex (last parts))]
+    (map #(.getPath %) (seq (.listFiles (io/as-file root)
+                                        (reify FilenameFilter
+                                          (accept [_ _ filename]
+                                            (boolean (re-find regex 
+                                                              filename)))))))))
+
+(defn- iterzip
+  "Iterate over a zip, returns a sequence of the nodes with a nil suffix"
+  [z]
+  (when-not (zip/end? z)
+    (cons (zip/node z) (lazy-seq (iterzip (zip/next z))))))
+
+(defn- f-dir? [f]
+  (when f (.isDirectory f)))
+
+(defn- f-children [f]
+  (.listFiles f))
+
+(defn- f-base [f]
+  (.getName f))
+
+(defn- iterdir* [path]
+  (let [root (io/as-file path)
+        nodes (butlast (iterzip (zip/zipper f-dir? f-children nil root)))]
+    (filter f-dir? nodes)))
+
+(defn- walk-map-fn [root]
+  (let [kids (f-children root)
+        dirs (set (map f-base (filter f-dir? kids)))
+        files (set (map f-base (filter (complement f-dir?) kids)))]
+    [(strinfify root) dirs files]))
+
+(defn iterdir
+  "Return a sequence [root dirs files], starting from path"
+  [path]
+  (map walk-map-fn (iterdir* path)))
+
+(defn walk
+  "Walk over directory structure. Calls 'func' with [root dirs files]"
+  [path func]
+  (dorun (map #(apply func %) (iterdir path))))
+
+(defn touch
+  "Set file modification time (default to now). Returns path."
+  [path & time]
+  (let [file (ensure-file path)]
+    (.setLastModified file (if time (first time) (System/currentTimeMillis)))
+    path))
+
+(defn chmod
+  "Change file permissions. Returns path.
+
+  'mode' can be any combination of \"r\" (readable) \"w\" (writable) and \"x\"
+  (executable). It should be prefixed with \"+\" to set or \"-\" to unset. And
+  optional prefix of \"u\" causes the permissions to be set for the owner only.
+  
+  Examples:
+  (chmod \"+x\" \"/tmp/foo\") -> Sets executable for everyone
+  (chmod \"u-wx\" \"/tmp/foo\") -> Unsets owner write and executable"
+  [mode path]
+  (assert-exists path)
+  (let [[_ u op permissions] (re-find #"^(u?)([+-])([rwx]{1,3})$" mode)]
+    (when (nil? op) (throw (IllegalArgumentException. "Bad mode")))
+    (let [perm-set (set permissions)
+          file (io/as-file path)
+          flag (= op "+")
+          user (not (empty? u))]
+      (when (perm-set \r) (.setReadable file flag user))
+      (when (perm-set \w) (.setWritable file flag user))
+      (when (perm-set \x) (.setExecutable file flag user)))
+    path))
+
+(defn copy+
+  "Copy src to dest, create directories if needed."
+  [src dest]
+  (mkdirs (dirname dest))
+  (copy src dest))
+
+(defn copy-tree
+  "Copy a directory from 'from' to 'to'."
+  [from to]
+  (when (file? to) 
+    (throw (IllegalArgumentException. (format "%s is a file" to))))
+  (let [from (normpath from)
+        to (normpath (if (exists? to) (join to (basename from)) to))
+        trim-size (inc (count from))
+        dest #(join to (subs % trim-size))]
+    (mkdirs to)
+    (walk from
+      (fn [root dirs files]
+        (dorun (map #(when-not (directory? %) (mkdirs (dest (join root %)))) 
+                    dirs))
+        (dorun (map #(copy+ (join root %) (dest (join root %))) files))))
+    to))
+
+(defn deltree
+  "Delete a directory tree."
+  [root]
+  (when (directory? root)
+    (dorun (map deltree (map #(join root %) (.list (io/as-file root))))))
+  (delete root))
+
+(defn home
+  "User home directory"
+  []
+  (System/getProperty "user.home"))
+
---
+++ test/fs_test.clj
@@ -0,0 +1,189 @@
+(ns fs-test
+  (:use [fs] :reload-all)
+  (:use [clojure.test]))
+
+(deftest listdir-test
+  (is (not (empty? (listdir ".")))))
+
+(deftest executable?-test
+  (is (executable? ".")))
+
+(deftest readable?-test
+  (is (readable? ".")))
+
+(deftest writeable?-test
+  (is (writeable? ".")))
+
+(deftest delete-test
+  (let [f (tempfile)]
+    (delete f)
+    (is (not (exists? f)))))
+
+(deftest exists?-test
+  (is (exists? ".")))
+
+; FIXME: This test sucks
+(deftest abspath-test
+  (is (> (count (abspath ".")) 2)))
+
+; FIXME: This test sucks
+(deftest normpath-test 
+  (is (> (count (normpath ".")) 2)))
+
+(deftest basename-test
+  (is (= (basename "/a/b/c") "c")))
+
+(deftest dirname-test
+  (is (= (dirname (join "a" "b" "c")) (join "a" "b"))))
+
+(deftest directory?-test
+  (is (directory? ".")))
+
+(deftest file?-test
+  (let [tmp (tempfile)]
+    (is (file? tmp))
+    (delete tmp)))
+
+; FIXME: This test sucks
+(deftest mtime-test
+  (let [tmp (tempfile)]
+    (is (> (mtime tmp) 0))
+    (delete tmp)))
+
+(deftest size-test
+  (let [f (tempfile)]
+    (spit f "abc")
+    (is (= (size f) 3))
+    (delete f)))
+
+(deftest mkdir-test
+  (let [f (tempfile)]
+    (delete f)
+    (mkdir f)
+    (is (directory? f))
+    (deltree f)))
+
+(deftest mkdirs-test
+  (let [f (tempfile)
+        sub (join f "a" "b")]
+    (delete f)
+    (mkdirs sub)
+    (is (directory? sub))
+    (deltree f)))
+
+(deftest join-test
+  (is (= (join "a" "b" "c") (apply str (interpose *separator* "abc")))))
+
+(deftest split-test
+  (is (= (split (apply str (interpose *separator* "abc"))) '("a" "b" "c"))))
+
+(deftest rename-test
+  (let [f (tempfile)
+        new-f (str f "-new")]
+    (rename f new-f)
+    (is (not (exists? f)))
+    (is (exists? new-f))
+    (delete new-f)))
+
+; FIXME: Test all variations of tempfile
+(deftest tempfile-test
+  (let [tmp (tempfile)]
+    (is (file? tmp))
+    (delete tmp)))
+
+; FIXME: Test all variations of tempdir
+(deftest tempdir-test
+  (let [tmp (tempdir)]
+    (is (directory? tmp))
+    (deltree tmp)))
+
+; FIXME: This test sucks
+(deftest cwd-test
+  (is (> (count (cwd)) 3)))
+
+(defn create-walk-dir []
+  (let [root (tempdir)]
+    (mkdir (join root "a"))
+    (mkdir (join root "b"))
+    (spit (join root "1") "1")
+    (spit (join root "a" "2") "1")
+    (spit (join root "b" "3") "1")
+    root))
+
+(def walk-atom (atom #{}))
+
+(defn walk-fn [root dirs files]
+  (swap! walk-atom conj [root dirs files]))
+
+(deftest walk-test
+ (let [root (create-walk-dir)]
+   (walk root walk-fn)
+   (let [result @walk-atom]
+     (is (= result
+            #{[root #{"b" "a"} #{"1"}]
+              [(join root "a") #{} #{"2"}]
+              [(join root "b") #{} #{"3"}]}))
+     (deltree root))))
+
+(deftest copy-test
+  (let [from (tempfile)
+        to (tempfile)
+        data "What's up Doc?"]
+    (delete to)
+    (spit from data)
+    (copy from to)
+    (is (= (slurp from) (slurp to)))
+    (delete from)
+    (delete to)))
+
+(deftest touch-test
+    (let [f (tempfile)
+          t (mtime f)]
+      (Thread/sleep 1000)
+      (touch f)
+      (is (> (mtime f) t))
+      (let [t1 3000]
+        (touch f t1)
+        (is (= (mtime f) t1)))
+      (delete f)))
+
+(deftest test-chmod
+  (let [f (tempfile)]
+    (chmod "-x" f)
+    (is (not (executable? f)))
+    (chmod "+x" f)
+    (is (executable? f))
+    (delete f)))
+
+(deftest test-copy-tree
+  (let [from (create-walk-dir)
+        to (tempdir)]
+    (swap! walk-atom (fn [_] #{}))
+    (let [path (copy-tree from to)
+          dest (join to (basename from))]
+      (is (= path dest))
+      (walk to walk-fn)
+      (let [result @walk-atom]
+        (is (= result
+               #{[to #{(basename from)} #{}]
+                 [dest #{"b" "a"} #{"1"}]
+                 [(join dest "a") #{} #{"2"}]
+                 [(join dest "b") #{} #{"3"}]})))
+      (deltree from)
+      (deltree to))))
+
+(deftest test-deltree
+  (let [root (create-walk-dir)
+        result (deltree root)]
+    (is (not (exists? root)))
+    (is (= root result))))
+
+(deftest test-home
+  (is (= (home) (System/getenv "HOME"))))
+
+(deftest text-extension
+  (is (= (extension "fs.clj") ".clj"))
+  (is (= (extension "fs.") "."))
+  (is (= (extension "/path/to/fs") ""))
+  (is (= (extension "fs.clj.bak") ".bak"))
+  (is (= (extension "") "")))
