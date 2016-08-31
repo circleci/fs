a82a1d413a87e36a0aea6a4fccb948ab2f4e44a3
Frank Wang <frank.wangtao@gmail.com>
2016-08-31 14:04:44 -0400

    Bump version and add circleci maven group id

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fs "0.11.1"
+(defproject circleci/fs "0.11.2"
   :description "File system utilities for clojure"
   :url "https://bitbucket.org/tebeka/fs/src"
   :dependencies [[org.clojure/clojure "1.3.0"]])
